643519d0fa008180bc711ee34c5c013f07e88551
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-03-23 23:05:59 -0300

    criacao de endpoint

--- src/main/java/com/example/spring/consumer/SpringConsumerApplication.java
+++ src/main/java/com/example/spring/consumer/SpringConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRabbit
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan("com.example.spring")
 public class SpringConsumerApplication {
---
+++ src/main/java/com/example/spring/consumer/api/AmqpApi.java
@@ -0,0 +1,22 @@
+package com.example.spring.consumer.api;
+
+import com.example.spring.consumer.service.RePublishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AmqpApi {
+
+    @Autowired
+    private RePublishService service;
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @GetMapping("/repost")
+    public void sendToQueue() {
+        service.repost();
+    }
+
+}
